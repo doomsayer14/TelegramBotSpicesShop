77d748ca79eff31c135af41cda6cd739e16ad7b0
vyesman <viktor.yesman@ukr.net>
2025-08-24 21:59:50 +0300

    Think of entities.

--- src/main/java/yes/shief/telegrambotspicesshop/entity/Customer.java
+++ src/main/java/yes/shief/telegrambotspicesshop/entity/Customer.java
@@ -4,21 +4,24 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Table(name = "order")
+@Table(name = "customer")
 @Data
 @Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Order {
+public class Customer {
     @Id
     private Long id;
 
     @Column
+    private String firstName;
 
-
-
-
+    @Column
+    private String secondName;
 }
--- src/main/java/yes/shief/telegrambotspicesshop/entity/OrderItem.java
+++ src/main/java/yes/shief/telegrambotspicesshop/entity/OrderItem.java
@@ -1,24 +1,17 @@
 package yes.shief.telegrambotspicesshop.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
 
-@Table(name = "order")
+@Table(name = "order_item")
 @Data
 @Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Order {
+public class OrderItem {
     @Id
     private Long id;
-
-    @Column
-
-
-
-
 }
